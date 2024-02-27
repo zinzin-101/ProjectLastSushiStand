489e4436f3d8cb18d7fd65bd00c7c7104a4c3922
nongpann <papran.tan@gmail.com>
2024-02-27 20:41:10 +0700

    Drone & Prefabs
    Code Drone
    Prefabs Cover, Enemy, Drone

--- Assets/Scripts/Enemy/DroneBrain.cs
+++ Assets/Scripts/Enemy/DroneBrain.cs
@@ -1,21 +1,77 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 namespace Enemy
 {
     public class DroneBrain : MonoBehaviour
     {
-        private EnemyShooter enemyShooter;
-
+        [SerializeField] private Transform player;
+        public Transform shootPoint;
+        private EnemyShooter shooter;
+        [SerializeField] private Transform center;
+        private int RandomposDelay = 1;
+        private float speed = 0.01f;
+        private Vector3 targetPos;
+        private int shootingDistance = 5;
         private void Awake()
         {
-            enemyShooter = GetComponent<EnemyShooter>();
+            shooter = GetComponent<EnemyShooter>();
         }
 
         private void Update()
         {
-            enemyShooter.Shoot();
+            LookAtTarget();
+            if (Vector3.Distance(transform.position, player.position) <= shootingDistance)
+            {
+                shooter.Shoot();
+            }
+
+
+            if (RandomposDelay == 1)
+            {
+                Debug.Log("RandomPos");
+                RandomPos();
+                RandomposDelay = 0;
+            }
+            if (targetPos != null)
+            {
+                Move();
+            }
+
+            if (transform.position == targetPos)
+            {
+                RandomposDelay = 1;
+            }
+        }
+
+        private void LookAtTarget()
+        {
+            Vector3 lookPos = player.position - transform.position;
+            Quaternion rotation = Quaternion.LookRotation(lookPos);
+            transform.rotation = Quaternion.Slerp(transform.rotation, rotation, 0.2f);
+            Debug.DrawLine(shootPoint.position, player.position, Color.blue, 0f);
+        }
+
+        private void RandomPos()
+        {
+            float x = Random.Range(-2.0f, 2.0f);
+            float z = Random.Range(-2.0f, 2.0f);
+            targetPos = new Vector3(center.position.x + x, center.position.y, center.position.z + z);
+            Debug.Log(targetPos);
+        }
+
+        private void Move()
+        {
+            transform.position = Vector3.MoveTowards(transform.position, targetPos, speed);
+        }
+
+        //debugZone
+        private void OnDrawGizmos()
+        {
+            Gizmos.color = Color.yellow;
+            Gizmos.DrawCube(center.position, new Vector3(4, 0, 4));
         }
     }
 }
